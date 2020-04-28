609be8482f076e89b6b4f626c7bf4dde574e220d
huangfu <huangfusuper@163.com>
2020-04-28 15:24:51 +0800

    完成登录页面及接口测试

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,20 @@
 <template>
     <div>
-      <h1>主页</h1>
+      <h1>token:</h1>
+      <h1>{{this.$store.getters.getLoginToken}}</h1>
+      <el-button @click='click' type="primary">测试</el-button>
     </div>
 </template>
 
 <script>
 export default {
-  name: 'Home'
+  name: 'Home',
+  methods: {
+    async click () {
+      const { res: data } = await this.$http.get('test')
+      console.log(data)
+    }
+  }
 }
 </script>
 
